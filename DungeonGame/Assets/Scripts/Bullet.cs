e287b54973387b2878f8d535dff0008e1b9637fc
Depthperception2166 <jwm2166@gmail.com>
2020-01-18 19:24:23 -0600

    Enemy Avoidance

--- DungeonGame/Assets/Scripts/Bullet.cs
+++ DungeonGame/Assets/Scripts/Bullet.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject);
+        if (collision.tag != "Bullet")
+        Destroy(this.gameObject);
     }
 }
--- DungeonGame/Assets/Scripts/Enemy.cs
+++ DungeonGame/Assets/Scripts/Enemy.cs
@@ -9,7 +9,11 @@ public class Enemy : Actor
 {
     public Rigidbody2D player;
     public GameObject bullet;
-    
+    public Transform firePoint;
+    public float bulletForce = 10f;
+
+    private float minDist = 1f;
+
 
     public float shootTimer = 2f;
     private float startShootTimer;
@@ -18,16 +22,22 @@ public class Enemy : Actor
     private float xDir;
     private float yDir;
     private Vector2 movementBias;
-    private float biasStrength = 4f;
-    
+
+    public float biasStrength = 1f;
+    public float radius = 5f;
+
     private void Start()
     {
         movementBias = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * biasStrength;
+
         startShootTimer = shootTimer;
+        
     }
 
     private void Update()
-    { 
+    {
+        
+
         //shoot
         shootTimer -= Time.deltaTime;
         if (shootTimer <= 0)
@@ -38,26 +48,59 @@ public class Enemy : Actor
         
     }
 
+    private Vector2 Avoid()
+    {
+        Vector2 avoidDir = Vector2.zero;
+
+
+        Collider2D selfCollider = GetComponent<Collider2D>();
+
+        //Find all colliders near enemy
+        var enemies = Physics2D.OverlapCircleAll(transform.position, radius, 9).ToList();
+        if (enemies.Contains(selfCollider))
+        {
+            enemies.Remove(selfCollider);
+        }
+
+        List<float> distances = new List<float>();
+        //loop through all enemies to find distance from this enemy
+        foreach(var enemy in enemies)
+        {
+            distances.Add(Vector2.Distance(transform.position, enemy.transform.position));
+        }
+        if (distances.Count != 0)
+        {
+            var shortestDistance = distances.Min();
+            var index = distances.IndexOf(shortestDistance);
+            var positionOfClosestEnemy = enemies[index].transform.position;
+
+            if (shortestDistance < minDist)
+            {
+                //Steer away from nearby enemy!
+                avoidDir = transform.position - positionOfClosestEnemy;
+                avoidDir = avoidDir.normalized * (1 - shortestDistance / minDist);
+                return avoidDir;
+            }
+        }
+
+
+        return avoidDir;
+
+    }
+
     private void FixedUpdate()
     {
+        //Movement
         xDir = (player.transform.position.x - transform.position.x);
         yDir = (player.transform.position.y - transform.position.x);
         Aim(player.transform.position);
-        Move(new Vector2(xDir, yDir) + movementBias);
+        Move(new Vector2(xDir, yDir) + movementBias + Avoid());
+        
     }
-
     private void Shoot()
     {
-        GameObject newBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+        GameObject newBullet = Instantiate(bullet, firePoint.transform.position, Quaternion.identity);
         Rigidbody2D rb = newBullet.GetComponent<Rigidbody2D>();
-        rb.AddForce((player.transform.position - transform.position) * 5f, ForceMode2D.Impulse);
+        rb.AddForce((player.transform.position - transform.position) * bulletForce, ForceMode2D.Impulse);
     }
-
-    /*private void OnTriggerEnter2D(Collider2D collision)
-    {
-        xDir = -xDir;
-        yDir = -yDir;
-    } */
-
-
 }
