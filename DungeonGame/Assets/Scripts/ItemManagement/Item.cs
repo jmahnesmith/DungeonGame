bb519fea60c037287c4298abb041ab3497ed0e8c
Depthperception2166 <jwm2166@gmail.com>
2020-02-02 15:14:27 -0600

    Adding items

---
+++ DungeonGame/Assets/Scripts/ItemManagement/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Item
+{
+    public int itemID;
+    public string itemName;
+    public string itemDescription;
+    public Sprite itemSprite;
+
+    public enum ItemType
+    {
+        None,
+        Consumable,
+        Equiptable
+    }
+    public ItemType itemType;
+}
